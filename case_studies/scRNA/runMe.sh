6a45fc8349efd7e0b0a7a88275f2136bb96927c3
Allen Kao <hushkao@gmail.com>
2024-07-20 00:31:07 +0800

    chore: Update scRNA download URL and command
    - Update download URL to `cellranger-8.0.1.tar.gz`
    - Change download command from `curl` to `wget`

--- case_studies/scRNA/runMe.sh
+++ case_studies/scRNA/runMe.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-
-curl --location --fail https://service.azul.data.humancellatlas.org/manifest/files/ksQwlKVkY3AzOaRjdXJsxBAQ8SWTo8FTpKjSwk0vFvAdxBA_kI4_i05VQ7H5Zurcs6oqxCBLMmxG5y7P6p0HM3uXlJKjETPuFIthphnhWe851fJL3Q \
-  | curl --fail-early --continue-at - --retry 15 --retry-delay 10 --config -
+# Download the cellranger-8.0.1.tar.gz file from the specified URL
+wget -O cellranger-8.0.1.tar.gz \
+  "https://cf.10xgenomics.com/releases/cell-exp/cellranger-8.0.1.tar.gz?Expires=1721449718&Key-Pair-Id=APKAI7S6A5RYOXBWRPDA&Signature=iD8KiXRVmnegl~HthVrAk~tUZEDYTGa~bhldJVI08dZ6dXNeoCqy-2DjuFQDHTrw2mU~GR9LWfecyKyv9hIHg1giU5wmWEgv~UGycoTGQ5ntZb~ISQ3fjiLAMLLs2KVEDnS4iX13M9CAcUvJeULTq0s-KJ97GK9-mPBDfzH18Ky3uGJK~Pr3WAZ-YdJ1rN0rE315dl8M9EVacW89v5L5gYCf34vUX0Wjki9SLSJYD1jcNULTCVjZytllY8zgyviEYG4pv31X3sPQDKnl7Kofi3zn395eybwnw0qQWOgPZENTEFur4DdNfTdUqu6e0jF3Bnk0tZe486mCmpNFjcljQg__"
